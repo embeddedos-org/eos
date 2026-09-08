8986bba592e8962392fdd8a373d59be926fefd29
Srikanth Patchava <srikanth.patchava@outlook.com>
2026-09-07 17:57:19 -0700

    Revert "Revert "chore: untrack the sanitizer build trees committed in #110"" (#141)
    Revert "Revert "chore: untrack the sanitizer build trees committed in #110" (…"
    
    This reverts commit 1988d85470f1b4d45629c14dd2d57bce4715f4b6.

--- bsan/tests/fuzz/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kartikey/Desktop/task/eos/tests/fuzz
-# Build directory: /Users/kartikey/Desktop/task/eos/bsan/tests/fuzz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
